4065523fc60eabce093f648295145e17d0d45367
Schmilly <william.f.s.bake@gmail.com>
2022-09-09 21:06:20 +0800

    Couintued work in setting up operations and alogirthims, still haven't got the file read to work yet unfortunatly

--- project1.c
+++ project1.c
@@ -7,6 +7,7 @@
 #define Estfilesize 10000
 #define Crontabsize 10000
 
+//**CURRENTLY DOESN'T WORK, NEED TO FIGURE OUT WHY AND HOW TO GET IT TO WORK
 void error_check(char filename) {
   // opening file of name passed via parameter of filename
   int file = open(filename, O_RDONLY);
@@ -16,19 +17,34 @@ void error_check(char filename) {
   }
 }
 
+// use with if statment for if it's the string version, we'll relie on string
+// version for this stuff I rekon
+void which_month(char month) {
+  // TODO
+}
+
 void reading_Estfil(char estfile) {
-  // TO DO
+  // TODO
   char buffer[Estfilesize];
 }
 
 void reading_CrONTABfile(char Crontab) {
-  // TO DO
+  // TODO
   char buffer[Crontabsize];
 }
 
+//*****MAIN FUNCTION V. IMPORTANT HOPEFLY THIS HELPS U FIND*****
 int main(int argcount, char *argvalue[]) {
-  // MAIN THINGY
   // defining parameters to pass to functions
+  if (argcount > 3) {
+    printf("Error, too many arguments");
+    exit(EXIT_FAILURE);
+  }
+  if (argcount < 3) {
+    printf("Error, not enough arguments");
+    exit(EXIT_FAILURE);
+  }
+
   char month = atof(argvalue[1]);
   char Crontab = atof(argvalue[2]);
   char estfile = atof(argvalue[3]);
