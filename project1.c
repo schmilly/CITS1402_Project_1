c48340579805dc514a244e3c9fa16523d010ccb1
Joshua <joshua@MSI.localdomain>
2022-09-12 22:43:36 +0800

    did a tiny bit of reading and putting into an array, figuring out if there's a simpler way

--- project1.c
+++ project1.c
@@ -5,7 +5,9 @@
 
 // defining size of chrachter array for files
 #define Estfilesize 10000
+
 #define Crontabsize 10000
+#define Crontablen 1000
 
 //**CURRENTLY DOESN'T WORK, NEED TO FIGURE OUT WHY AND HOW TO GET IT TO WORK
 void error_check(char filename) {
@@ -25,13 +27,46 @@ void which_month(char month) {
 
 void reading_Estfil(char estfile) {
   // TODO
-  char buffer[Estfilesize];
-}
+  char buffer[Estfilesize]
 
+	
+}
+//just did some file error and opening stuff. converted it into an array of string
 void reading_CrONTABfile(char Crontab) {
   // TODO
-  char buffer[Crontabsize];
+  char data[Crontabsize][Crontablen]
+
+  FILE *file;
+
+  file = fopen("file.txt", "r");
+
+  if (file == NULL)
+  {
+	  printf("Error opening file.\n");
+	  return 1;
+  }
+
+  int line = 0;
+
+  while(!feof(file) && ferror(file))
+	  if(fgets(data[line], Crontablen, file) != NULL)
+		  line++;
+  fclose(file);
+
+  for (int i = 0; i < line; i++)
+	  printf("%s", data[i])
+
+  return 0;
 }
+
+
+//setup counter maybe in the reading function or possibly its own function
+//deciding which is less messy
+//thiking of making an array for the counter, so we can use a for loop possibly to determine which one occured the most
+//may be easier way not too sure
+//not too sure how to determine max no. of commands running at any time
+
+
 
 //*****MAIN FUNCTION V. IMPORTANT HOPEFLY THIS HELPS U FIND*****
 int main(int argcount, char *argvalue[]) {
